64510cc0f97eb089d8630b108dc7d208e2ae74eb
Amaresh Ray <amareshray@gmail.com>
2013-02-23 13:19:56 +0000

    initialised

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Affzero::Application.config.secret_token = '2fa068fbb2f1e0a35f61b76fe26b431348b690b5db600b6f7887c67ea0823979e5d741d3c600a202bdd4e68e434d5b87d9e4a9d8eb2f2413af48d34c57fd8d1a'
